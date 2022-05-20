caf902be1cc08da2c4eeae0df4d1fe0b437a0846
helberth <blitzart@gmail.com>
2022-05-20 17:46:48 -0400

    fix linter errors

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   },
   rules: {
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    quotes: ["error", "double"],
   },
 };
--- api/src/fakes/book.fake.js
+++ api/src/fakes/book.fake.js
@@ -1,4 +1,4 @@
-const { faker } = require("@faker-js/faker");
+const { faker } = require('@faker-js/faker');
 
 const generateOneBook = () => ({
   _id: faker.datatype.uuid(),
